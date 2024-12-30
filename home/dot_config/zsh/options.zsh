850c2422d971f5b1c5e4003ce17eb6c87d261884
jarodtaylor <jarodrtaylor@gmail.com>
2024-12-29 21:52:48 -0600

    updated zsh options and cleaned up some more files

--- home/dot_config/zsh/options.zsh
+++ home/dot_config/zsh/options.zsh
@@ -1,85 +1,47 @@
-# https://gist.github.com/mattmc3/c490d01751d6eb80aa541711ab1d54b1
-
-## ZSH Options
-# http://zsh.sourceforge.net/Doc/Release/Options.html
-
-# Changing Directories
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Changing-Directories
-setopt auto_cd                 # if a command isn't valid, but is a directory, cd to that dir
-setopt auto_pushd              # make cd push the old directory onto the directory stack
-setopt pushd_ignore_dups       # don’t push multiple copies of the same directory onto the directory stack
-setopt pushd_minus             # exchanges the meanings of ‘+’ and ‘-’ when specifying a directory in the stack
-
-# Completions
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Completion-2
-setopt always_to_end           # move cursor to the end of a completed word
-setopt auto_list               # automatically list choices on ambiguous completion
-setopt auto_menu               # show completion menu on a successive tab press
-setopt auto_param_slash        # if completed parameter is a directory, add a trailing slash
-setopt complete_in_word        # complete from both ends of a word
-unsetopt menu_complete         # don't autoselect the first completion entry
-
-# Expansion and Globbing
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Expansion-and-Globbing
-setopt extended_glob           # use more awesome globbing features
-setopt glob_dots               # include dotfiles when globbing
+#
+# Core ZSH Options
+# Focused on developer workflow and terminal convenience
+#
 
 # History
-# http://zsh.sourceforge.net/Doc/Release/Options.html#History
-setopt append_history          # append to history file
-setopt extended_history        # write the history file in the ':start:elapsed;command' format
-unsetopt hist_beep             # don't beep when attempting to access a missing history entry
-setopt hist_expire_dups_first  # expire a duplicate event first when trimming history
-setopt hist_find_no_dups       # don't display a previously found event
-setopt hist_ignore_all_dups    # delete an old recorded event if a new event is a duplicate
-setopt hist_ignore_dups        # don't record an event that was just recorded again
-setopt hist_ignore_space       # don't record an event starting with a space
-setopt hist_no_store           # don't store history commands
-setopt hist_reduce_blanks      # remove superfluous blanks from each command line being added to the history list
-setopt hist_save_no_dups       # don't write a duplicate event to the history file
-setopt hist_verify             # don't execute immediately upon history expansion
-setopt inc_append_history      # write to the history file immediately, not when the shell exits
-unsetopt share_history         # don't share history between all sessions
+setopt extended_history        # Save timestamp and duration
+setopt hist_ignore_all_dups   # Remove older duplicate entries from history
+setopt hist_ignore_space      # Don't record commands starting with space
+setopt hist_verify            # Show command with history expansion before running it
+setopt share_history          # Share history between all sessions
+setopt inc_append_history     # Add commands to history as they're typed
+setopt hist_reduce_blanks     # Remove superfluous blanks from history
 
-# Initialization
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Initialisation
+# Directory Navigation
+setopt auto_cd               # If command is a directory path, cd into it
+setopt auto_pushd           # Push the old directory onto the stack on cd
+setopt pushd_ignore_dups    # Don't store duplicates in the stack
+setopt pushd_minus          # Use +/- operators for pushd
+
+# Completion
+setopt auto_menu            # Show completion menu on tab press
+setopt complete_in_word     # Complete from both ends of a word
+setopt always_to_end        # Move cursor to end of word after completion
+unsetopt menu_complete      # Don't autoselect the first completion entry
 
 # Input/Output
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Input_002fOutput
-unsetopt clobber               # must use >| to truncate existing files
-unsetopt correct               # don't try to correct the spelling of commands
-unsetopt correct_all           # don't try to correct the spelling of all arguments in a line
-unsetopt flow_control          # disable start/stop characters in shell editor
-setopt interactive_comments    # enable comments in interactive shell
-unsetopt mail_warning          # don't print a warning message if a mail file has been accessed
-setopt path_dirs               # perform path search even on command names with slashes
-setopt rc_quotes               # allow 'Henry''s Garage' instead of 'Henry'\''s Garage'
-unsetopt rm_star_silent        # ask for confirmation for `rm *' or `rm path/*'
+setopt interactive_comments  # Allow comments in interactive shell
+unsetopt beep              # No beep on error
+unsetopt flow_control      # Disable start/stop characters (^S/^Q)
+
+# Globbing and Files
+setopt extended_glob        # Use extended globbing syntax
+setopt glob_dots           # Include hidden files in globbing
+unsetopt case_glob         # Make globbing case insensitive
 
 # Job Control
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Job-Control
-setopt auto_resume            # attempt to resume existing job before creating a new process
-unsetopt bg_nice              # don't run all background jobs at a lower priority
-unsetopt check_jobs           # don't report on jobs when shell exit
-unsetopt hup                  # don't kill jobs on shell exit
-setopt long_list_jobs         # list jobs in the long format by default
-setopt notify                 # report status of background jobs immediately
+setopt auto_resume         # Allow simple commands to resume background jobs
+setopt long_list_jobs      # List jobs in long format
+setopt notify             # Report status of background jobs immediately
 
-# Prompting
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Prompting
-setopt prompt_subst           # expand parameters in prompt variables
+# Safety
+unsetopt rm_star_silent    # Prompt before executing 'rm *'
+unsetopt clobber          # Don't overwrite files with >
 
-# Scripts and Functions
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Scripts-and-Functions
-
-# Shell Emulation
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Shell-Emulation
-
-# Shell State
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Shell-State
-
-# Zle
-# http://zsh.sourceforge.net/Doc/Release/Options.html#Zle
-unsetopt beep                 # be quiet!
-setopt combining_chars        # combine zero-length punctuation characters (accents) with the base character
-setopt emacs                  # use emacs keybindings in the shell
+# Command Editing
+setopt combining_chars     # Handle multi-byte characters
