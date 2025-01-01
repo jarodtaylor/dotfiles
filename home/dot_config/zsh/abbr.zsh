d90fd9aaf878aeeb30c80213840fffcb533b6187
Jarod Taylor <jarodrtaylor@gmail.com>
2025-01-01 14:10:54 -0600

    more stuff

---
+++ home/dot_config/zsh/abbr.zsh
@@ -0,0 +1,9 @@
+## Git
+abbr lg="lazygit"
+abbr g="git "
+abbr gs="git status"
+abbr gss="git status --short"
+abbr gsb="git status --short --branch"
+abbr gl="git log --graph -all"
+abbr gd="git diff"
+abbr gds="git diff --staged"
