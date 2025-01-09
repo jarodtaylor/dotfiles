b5c608db2dcc590324d1774c651db9ecf69b794f
Jarod Taylor <jarodrtaylor@gmail.com>
2025-01-09 00:13:46 -0600

    fixed the guy fawkes ascii

--- home/dot_config/nvim/lua/jarod/plugins/alpha.lua
+++ home/dot_config/nvim/lua/jarod/plugins/alpha.lua
@@ -4,30 +4,19 @@ return {
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
-    dashboard.section.header.val = {
-      "                                ",
-      "                                ",
-      "                                ",
-      "     ⣀⣀⣤⣤⣴⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣦⣤⣤⣄⣀⡀   ",
-      "   ⣴⣿⣿⡿⣿⢿⣟⣿⣻⣟⡿⣟⣿⣟⡿⣟⣿⣻⣟⣿⣻⢿⣻⡿⣿⢿⣷⣆ ",
-      "  ⢘⣿⢯⣷⡿⡿⡿⢿⢿⣷⣯⡿⣽⣞⣷⣻⢯⣷⣻⣾⡿⡿⢿⢿⢿⢯⣟⣞⡮⡀",
-      "  ⢸⢞⠟⠃⣉⢉⠉⠉⠓⠫⢿⣿⣷⢷⣻⣞⣿⣾⡟⠽⠚⠊⠉⠉⠉⠙⠻⣞⢵⠂",
-      "  ⢜⢯⣺⢿⣻⣿⣿⣷⣔⡄⠄⠈⠛⣿⣿⡾⠋⠁⠄⠄⣄⣶⣾⣿⡿⣿⡳⡌⡗⡅",
-      "  ⢽⢱⢳⢹⡪⡞⠮⠯⢯⡻⡬⡐⢨⢿⣿⣿⢀⠐⡥⣻⡻⠯⡳⢳⢹⢜⢜⢜⢎⠆",
-      "  ⣻⢌⠘⠌⡂⠈⠁⠉⠁⠘⠑⢧⣕⣿⣿⣿⢤⡪⠚⠂⠈⠁⠁⠁⠂⡑⠡⡈⢮⠅",
-      "  ⣳⣿⣿⣽⣭⣶⣶⣶⣶⣶⣺⣟⣾⣻⣿⣯⢯⢿⣳⣶⣶⣶⣖⣶⣮⣭⣷⣽⣗⠍",
-      "  ⢻⡿⡿⣟⣿⣻⣽⣟⣿⢯⣟⣞⡷⣿⣿⣯⢿⢽⢯⣿⣻⣟⣿⣻⣟⣿⣻⢿⣿⢀",
-      "  ⡑⡏⠯⡯⡳⡯⣗⢯⢟⡽⣗⣯⣟⣿⣿⣾⣫⢿⣽⠾⡽⣺⢳⡫⡞⡗⡝⢕⠕⠄",
-      "  ⢂⡎⠅⡃⢇⠇⠇⣃⣧⡺⡻⡳⡫⣿⡿⣟⠞⠽⠯⢧⣅⣃⠣⠱⡑⡑⠨⢐⢌⠂",
-      "  ⠐⠼⣦⢀⠄⣶⣿⢿⣿⣧⣄⡌⠂⠢⠩⠂⠑⣁⣅⣾⢿⣟⣷⠦⠄⠄⡤⡇⡪⠄",
-      "  ⠨⢻⣧⡅⡈⠛⠿⠿⠿⠛⠁⠄⢀⡀⠄⠄⠘⠻⠿⠿⠯⠓⠁⢠⣱⡿⢑⠄⠄ ",
-      "   ⠈⢌⢿⣷⡐⠤⣀⣀⣂⣀⢀⢀⡓⠝⡂⡀⢀⢀⢀⣀⣀⠤⢊⣼⡟⡡⡁  ",
-      "    ⠈⢢⠚⣿⣄⠈⠉⠛⠛⠟⠿⠿⠟⠿⠻⠻⠛⠛⠉⠄⣠⠾⢑⠰⠈   ",
-      "      ⠑⢌⠿⣦⡡⣱⣸⣸⣆⠄⠄⠄⣰⣕⢔⢔⠡⣼⠞⡡⠁⠁    ",
-      "        ⠑⢝⢷⣕⡷⣿⡿⠄⠄⠠⣿⣯⣯⡳⡽⡋⠌       ",
-      "          ⠙⢮⣿⣽⣯⠄⠄⢨⣿⣿⡷⡫⠃         ",
-      "            ⠘⠙⠝⠂⠄⢘⠋⠃⠁           ",
-    }
+    
+    local guy_fawkes = require("jarod.plugins.alpha-headers.guy-fawkes")
+    
+    -- Apply the highlights
+    for group, spec in pairs(guy_fawkes.highlights) do
+      vim.api.nvim_set_hl(0, group, spec)
+    end
+    
+    -- Set the header
+    dashboard.section.header.val = guy_fawkes.header
+    dashboard.section.header.opts = guy_fawkes.opts
+
+    -- Rest of your existing configuration
     dashboard.section.buttons.val = {
       dashboard.button("e", "  New File", "<cmd>ene<CR>"),
       dashboard.button("ee", "  Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
@@ -35,11 +24,11 @@ return {
       dashboard.button("fg", "  Find Word", "<cmd>Telescope live_grep<CR>"),
       dashboard.button("wr", "󰁯  Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
       dashboard.button("q", "  Quit NVIM", "<cmd>qa<CR>"),
-    }
-    -- Send config to alpha
-    alpha.setup(dashboard.opts)
+    }  
 
-    -- Disable folding on alpha buffer
+    alpha.setup(dashboard.opts)
     vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
   end,
 }
+
+   
