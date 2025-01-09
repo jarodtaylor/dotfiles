3570069003237f8b9b5003288be97dc6cb865851
Jarod Taylor <jarodrtaylor@gmail.com>
2025-01-09 00:35:20 -0600

    working on alpha colors

--- home/dot_config/nvim/lua/jarod/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/jarod/plugins/colorscheme.lua
@@ -10,15 +10,9 @@ return {
       flavour = "mocha", -- latte, frappe, macchiato, mocha
       transparent_background = true, -- disables setting the background color.
       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      custom_highlights = function(colors)
-        return {
-          AlphaShortcut = { fg = colors.green },
-          AlphaHeader = { fg = colors.blue },
-          AlphaHeaderLabel = { fg = colors.peach },
-          AlphaButtons = { fg = colors.lavender },
-          AlphaFooter = { fg = colors.yellow, style = { "italic" } },
-        }
-      end
+      integrations = {
+        alpha = false,
+      }
     })
 
     vim.cmd.colorscheme("catppuccin")
