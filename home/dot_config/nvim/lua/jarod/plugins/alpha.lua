a1395804d2a51864d6f9dece3a9301da5eb4370a
Jarod Taylor <jarodrtaylor@gmail.com>
2025-01-10 15:27:09 -0600

    committing before trying nvim colorscheme tweak

--- home/dot_config/nvim/lua/jarod/plugins/alpha.lua
+++ home/dot_config/nvim/lua/jarod/plugins/alpha.lua
@@ -1,22 +1,28 @@
 return {
   "goolord/alpha-nvim",
   event = "VimEnter",
+  -- dependencies = {
+  --   "catppuccin/nvim",
+  -- },
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
-    
-    local guy_fawkes = require("jarod.plugins.alpha-headers.guy-fawkes")
-    
-    -- Apply the highlights
-    for group, spec in pairs(guy_fawkes.highlights) do
-      vim.api.nvim_set_hl(0, group, spec)
+    local utils = require("jarod.utils")
+    -- local mocha = require("catppuccin.paletttes").get_palette "mocha"
+    -- local header = require("jarod.plugins.alpha-headers.neovim")
+    local function footer()
+      local plugins_count = utils.get_lazy_plugin_count()
+      local datetime = os.date("  %m-%d-%Y   %H:%M")
+      local version = vim.version()
+      local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+      -- local fortune = require("alpha.fortune")
+      return datetime .. "   Plugins " .. plugins_count .. nvim_version_info
     end
-    
     -- Set the header
-    dashboard.section.header.val = guy_fawkes.header
-    dashboard.section.header.opts = guy_fawkes.opts
+    --dashboard.section.header.val = header.ascii
+    dashboard.section.header.val = "Jarod is awesome!"
+    -- dashboard.section.header.opts.hl = "red"
 
-    -- Rest of your existing configuration
     dashboard.section.buttons.val = {
       dashboard.button("e", "  New File", "<cmd>ene<CR>"),
       dashboard.button("ee", "  Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
@@ -24,11 +30,11 @@ return {
       dashboard.button("fg", "  Find Word", "<cmd>Telescope live_grep<CR>"),
       dashboard.button("wr", "󰁯  Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
       dashboard.button("q", "  Quit NVIM", "<cmd>qa<CR>"),
-    }  
+    }
+
+    dashboard.section.footer.val = footer()
 
     alpha.setup(dashboard.opts)
     vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
   end,
 }
-
-   
--- home/dot_config/nvim/lua/jarod/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/jarod/plugins/colorscheme.lua
@@ -1,20 +1,28 @@
-return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    local catppuccin = require("catppuccin")
-
-    catppuccin.setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      transparent_background = true, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      integrations = {
-        alpha = false,
-      }
-    })
-
-    vim.cmd.colorscheme("catppuccin")
-  end,
-}
+-- return {
+--   "catppuccin/nvim",
+--   lazy = false,
+--   name = "catppuccin",
+--   priority = 1000,
+--   config = function()
+--     local catppuccin = require("catppuccin")
+--     catppuccin.setup({
+--       flavour = "mocha", -- latte, frappe, macchiato, mocha
+--       transparent_background = true, -- disables setting the background color.
+--       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+--       custom_highlights = function(colors)
+--         return {
+--           AlphaShortcut = { fg = colors.blue },
+--           AlphaHeader = { fg = colors.blue },
+--           AlphaHeaderLabel = { fg = colors.blue },
+--           AlphaButtons = { fg = colors.blue },
+--           AlphaFooter = { fg = colors.blue, style = { "italic" } },
+--         }
+--       end,
+--       integrations = {
+--         alpha = false
+--       }
+--     })
+--
+--     vim.cmd.colorscheme("catppuccin")
+--   end,
+-- }
